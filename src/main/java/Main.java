ebc34da35aab3368fa688a86bfe3898ed408f28f
usernameMaciej <maciej.przystarz@protonmail.com>
2023-03-30 11:43:49 +0200

    Poprawa

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Main {
-    public static void main(String[] args) {
-        TransactionDao transactionDao = new TransactionDao();
-        transactionDao.run();
-    }
-}
--- src/main/java/TransactionDao.java
+++ src/main/java/TransactionDao.java
@@ -1,52 +1,24 @@
 import java.sql.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TransactionDao {
     private Connection connection;
-    private final Scanner scanner = new Scanner(System.in);
-    private Transaction transaction;
 
-    public void run() {
-        int userOption;
-        do {
-            System.out.println("Co chcesz zrobić?");
-            System.out.println(Arrays.toString(Option.values()));
-            userOption = scanner.nextInt();
-            scanner.nextLine();
-            chooseOption(userOption);
-        } while (userOption != Option.EXIT.id);
-    }
-
-    private void chooseOption(int userOption) {
-        Option option = Option.userOption(userOption);
-
-        switch (option) {
-            case EXIT -> System.out.println("Koniec programu");
-            case ADD -> add(transaction);
-            case MODIFY -> modify(transaction);
-            case DELETE -> delete(transaction.getId());
-            case SHOW_INCOME -> showIncome();
-            case SHOW_EXPENSE -> showExpense();
-            default -> System.out.println("Zły wybór opcji");
-        }
-    }
-
-    private void showExpense() {
+    public void showExpense() {
         connect();
         String sql = "SELECT * FROM home_budget.transaction WHERE type = 'wydatki'";
         printTransaction(sql);
         closeConnection();
     }
 
-    private void showIncome() {
+    public void showIncome() {
         connect();
         String sql = "SELECT * FROM home_budget.transaction WHERE type = 'przychody'";
         printTransaction(sql);
         closeConnection();
     }
 
-    private void printTransaction(String sql) {
+    public void printTransaction(String sql) {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -63,8 +35,14 @@ public class TransactionDao {
         }
     }
 
-    private void delete(Long id) {
+    public void delete() {
         connect();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj id transakcji, którą chcesz usunąć");
+        long id = scanner.nextLong();
+        scanner.nextLine();
+
         try {
             String sql = "DELETE FROM home_budget.transaction WHERE id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -76,29 +54,66 @@ public class TransactionDao {
         closeConnection();
     }
 
-    private void modify(Transaction transaction) {
+    public void modify() {
         connect();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj id transakcji, którą chcesz zaktualizować");
+        long id = scanner.nextLong();
+        scanner.nextLine();
+
+        System.out.println("Podaj typ (wydatki lub przychody)");
+        String type = scanner.nextLine();
+
+        System.out.println("Podaj opis");
+        String description = scanner.nextLine();
+
+        System.out.println("Podaj kwotę");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Podaj datę");
+        Date date = Date.valueOf(scanner.nextLine());
+
+        Transaction transaction = new Transaction(id, type, description, amount, date);
+
         try {
             String sql = "UPDATE home_budget.transaction SET type = ?, description = ?, amount = ?, date = ?";
-            prepareStatements(transaction, sql);
+            executeStatement(transaction, sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         closeConnection();
     }
 
-    private void add(Transaction transaction) {
+    public void add() {
         connect();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj typ transakcji (wydatki lub przychody)");
+        String type = scanner.nextLine();
+
+        System.out.println("Podaj opis transakcji");
+        String description = scanner.nextLine();
+
+        System.out.println("Podaj kwotę transakcji");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Podaj datę transakcji");
+        Date date = Date.valueOf(scanner.nextLine());
+
+        Transaction transaction = new Transaction(type, description, amount, date);
         try {
             String sql = "INSERT INTO home_budget.transaction(type, description, amount, date) VALUES (?, ?, ?, ?)";
-            prepareStatements(transaction, sql);
+            executeStatement(transaction, sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         closeConnection();
     }
 
-    private void prepareStatements(Transaction transaction, String sql) throws SQLException {
+    private void executeStatement(Transaction transaction, String sql) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, transaction.getType());
         preparedStatement.setString(2, transaction.getDescription());
@@ -126,27 +141,6 @@ public class TransactionDao {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/home_budget?serverTimezone=UTC", "root", "asd123");
         } catch (SQLException e) {
             System.err.println("Błąd podczas nawiązywania połączenia");
-        }
-    }
-
-    private enum Option {
-        EXIT(0, "Wyjście"), ADD(1, "Dodawanie transakcji"), MODIFY(2, "Modyfikacja transakcji"), DELETE(3, "Usuwanie transakcji"), SHOW_INCOME(4, "Wyświetlanie wszystkich przychodów"), SHOW_EXPENSE(5, "Wyświetlanie wszystkich wydatków");
-
-        private int id;
-        private String description;
-
-        Option(int id, String description) {
-            this.id = id;
-            this.description = description;
-        }
-
-        public static Option userOption(int userOption) {
-            return Option.values()[userOption];
-        }
-
-        @Override
-        public String toString() {
-            return id + " - " + description;
         }
     }
 }
